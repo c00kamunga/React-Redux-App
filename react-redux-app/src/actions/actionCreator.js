51dc038d04549130df4190f7b6acf31b79adce08
c00kamunga <tsothalanti2@yahoo.com>
2020-04-28 16:25:46 -0700

    created an API call to ghibli api, dispatch type FETCH_FILMS_SUCCESS in .then and dispatch type FETCH_FILMS_FAIL in .catch

--- react-redux-app/src/actions/actionCreator.js
+++ react-redux-app/src/actions/actionCreator.js
@@ -4,3 +4,13 @@ export const FETCH_FILMS_START = 'FETCH_FILMS_START';
 export const FETCH_FILMS_SUCCESS = 'FETCH_FILMS_SUCCESS';
 export const FETCH_FILMS_FAIL= 'FETCH_FILMS_FAIL';
 
+
+export const getFilms = () => dispatch => {
+    dispatch({ type: FETCH_FILMS_START});
+    axios
+    .get(`https://ghibliapi.herokuapp.com/#tag/Films`)
+    .then(res =>{
+        dispatch({ type: FETCH_FILMS_SUCCESS, payload: res.data.results})
+    })
+    .catch(err => dispatch ({ type: FETCH_FILMS_FAIL, payoad: err}))
+}
